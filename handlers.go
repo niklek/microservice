df1fecca1dfbb2bf3632e2dbd3368d55dedc39c8
Nik <nikita.leksikov@shpock.com>
2020-11-15 13:45:59 +0100

    feat: log service version and commit

--- handlers.go
+++ handlers.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"github.com/niklek/microservice/internal/version"
 	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
+// Home handler
 func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	log.WithFields(logrus.Fields{
-		"path": r.URL.Path,
+		"path":    r.URL.Path,
+		"version": version.Version,
+		"commit":  version.Commit,
 	}).Info("Requested home")
 	fmt.Fprintf(w, "Request path:%s\n", r.URL.Path)
 }
